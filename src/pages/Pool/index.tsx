cb0909828bd294e6b9e209169b7a75fc00ec7bbd
Moody Salem <moody.salem@gmail.com>
2021-07-23 03:56:32 +0200

    merge incentives with each pool

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -202,6 +202,7 @@ export default function Pool() {
   const [userHideClosedPositions, setUserHideClosedPositions] = useUserHideClosedPositions()
 
   const { positions, loading: positionsLoading } = useV3Positions(account)
+  console.log(positions)
 
   const { incentives, loading: incentivesLoading } = useAllIncentives()
 
