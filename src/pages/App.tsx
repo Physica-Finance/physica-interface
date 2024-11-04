Uniswap/interface

start LM ui (#2085)

src/pages/App.tsx
@@ -89,7 +89,7 @@ export default function App() {
               <Route exact strict path="/vote" component={Vote} />
               <Route exact strict path="/vote/:governorIndex/:id" component={VotePage} />
               <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
-              <Route exact strict path="/uni" component={Earn} />
+              <Route exact strict path="/earn" component={Earn} />
               <Route exact strict path="/uni/:currencyIdA/:currencyIdB" component={Manage} />
 
               <Route exact strict path="/program" component={CreateIncentive} />

Physica-Finance/physica-interface

src/pages/App.tsx
@@ -217,6 +217,8 @@ export default function App() {
                     </Suspense>
                   }
                 />
+                <Route exact strict path="/claim" component={OpenClaimAddressModalAndRedirectToSwap} />
+
                 <Route path="create-proposal" element={<Navigate to="/vote/create-proposal" replace />} />
 
                 <Route path="/program" element={<CreateIncentive />} />